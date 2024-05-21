91bd9cf033e6ae1f974786a128713d73951d1ecb
BerserKess <kesleyrainery.21@gmail.com>
2024-05-20 21:56:16 -0300

    completando calculos

--- js/script.js
+++ js/script.js
@@ -9,19 +9,40 @@ function calculate(ev){
     const number2 = document.getElementById('number2').value;
     const number3 = document.getElementById('number3').value;
 
-    let delta = Math.pow(number2,2) - 4 * number1 * number3;
-
-    if (delta > 0){
-        const rootOne = (-number2 + Math.sqrt(delta)) / (2 * number1);
-        const rootTwo = (-number2 - Math.sqrt(delta)) / (2 * number1);
-
-        resultDiv.innerHTML = `&#916 = ${delta}<br>X' = ${rootOne}<br> X'' = ${rootTwo}`;
-    } else if (delta === 0){
-        const root = (-number2 + Math.sqrt(delta)) / (2 * number1);
-        resultDiv.innerHTML = `&#916 = ${delta}<br><br>X = ${root}`;
-    } else if (delta < 0){
-        resultDiv.innerHTML = `&#916 = ${delta}<br><br>Não existem raízes no conjunto dos número reais.`;
+    if(number1 == 0){
+        alert(`Coeficiente A não pode ser 0`);
     }
+    else if (number2 == 0){
+        const incompleteRootOne =  Math.sqrt(-number3 / number1)
+        const incompleteRootTwo = -Math.sqrt(-number3 / number1)
+
+        resultDiv.innerHTML = `X' = ${incompleteRootOne}<br> X'' = ${incompleteRootTwo}`;
+    }
+    else if (number3 == 0){
+        const incompleteRootOne = 0
+        const incompleteRootTwo = -number2 / number1
+
+        resultDiv.innerHTML = `X' = ${incompleteRootOne}<br> X'' = ${incompleteRootTwo}`;
+    }
+    else{
+        let delta = Math.pow(number2,2) - 4 * number1 * number3;
+
+
+
+        if (delta > 0){
+            const rootOne = (-number2 + Math.sqrt(delta)) / (2 * number1);
+            const rootTwo = (-number2 - Math.sqrt(delta)) / (2 * number1);
+
+            resultDiv.innerHTML = `&#916 = ${delta}<br>X' = ${rootOne}<br> X'' = ${rootTwo}`;
+        } else if (delta === 0){
+            const root = (-number2 + Math.sqrt(delta)) / (2 * number1);
+            resultDiv.innerHTML = `&#916 = ${delta}<br><br>X = ${root}`;
+        } else if (delta < 0){
+            resultDiv.innerHTML = `&#916 = ${delta}<br><br>Não existem raízes no conjunto dos número reais.`;
+        }
+
+    }
+
 
 
 }
